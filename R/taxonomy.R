6ea0bb47fe49f46750744946a4ac6f76572355da
Matt Cannon <matthewvc1@gmail.com>
2025-10-03 10:40:02 +0000

    version 1.1.0

--- R/taxonomy.R
+++ R/taxonomy.R
@@ -7,16 +7,19 @@
 #' @return data.frame of the 'accessions, taxIds, and taxonomy
 #' @export
 
-get_taxonomy = function(accessions){
+get_taxonomy <- function(accessions) {
+    accessions <- unique(as.character(accessions))
+    taxids <- accession2taxid(accessions)
 
-  accessions = unique(as.character(accessions))
-  taxids = accession2taxid(accessions)
-
-  taxonomy=fetch_taxonomy(unique(taxids))
-  merge(
-    data.frame(accession=names(taxids), taxId=taxids, stringsAsFactors=FALSE),
-    taxonomy
-  )
+    taxonomy <- fetch_taxonomy(unique(taxids))
+    merge(
+        data.frame(
+            accession = names(taxids),
+            taxId = taxids,
+            stringsAsFactors = FALSE
+        ),
+        taxonomy
+    )
 }
 #' Maps a nucleotide database accession to a taxonomy database taxId
 #'
@@ -24,52 +27,69 @@ get_taxonomy = function(accessions){
 #' @return named vector of taxIds.
 #' @export
 
-accession2taxid = function(accessions){
-  url = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/elink.fcgi'
+accession2taxid <- function(accessions) {
+    url <- "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi"
 
-  names(accessions) = rep('id', times=length(accessions))
-  query=c(list(db='taxonomy', dbfrom='nuccore', idtype='acc'), accessions)
+    taxids <-
+        httr2::request(url) |>
+        httr2::req_method("POST") |>
+        httr2::req_body_form(
+            db = "nuccore",
+            id = paste0(accessions, collapse = ",")
+        ) |>
+        httr2::req_retry(max_tries = 5) |>
+        httr2::req_perform() |>
+        httr2::resp_body_string() |>
+        XML::xmlParse() |>
+        XML::xpathSApply(
+            "//DocSum",
+            parse_docsum
+        )
 
-  response=POST_retry(url, body=query)
-
-  #stop if response failed
-  stop_for_status(response)
-
-  parsed = xmlParse(content(response, as="text"))
-
-  xpathSApply(parsed, '//LinkSet', parse_LinkSet)
+    taxids
 }
-parse_LinkSet = function(LinkSet){
-  gid = xpathSApply(LinkSet, './/IdList/Id', xmlValue)
-  taxid = xpathSApply(LinkSet, './/LinkSetDb/Link/Id', xmlValue)
-  if(length(taxid) != 1)
-    taxid = NA
-  names(taxid) = gid
-  taxid
+parse_docsum <- function(docsum) {
+    gid <- XML::xpathSApply(
+        docsum,
+        ".//Item[@Name='AccessionVersion']", XML::xmlValue
+    )
+    taxid <- XML::xpathSApply(
+        docsum,
+        ".//Item[@Name='TaxId']", XML::xmlValue
+    )
+    if (length(taxid) != 1) {
+        taxid <- NA
+    }
+    names(taxid) <- gid
+    taxid
 }
 
-fetch_taxonomy = function(taxid) {
+fetch_taxonomy <- function(taxid) {
+    fetch_url <- "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi"
 
-  fetch_url = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi'
+    query <- list(
+        db = "taxonomy",
+        rettype = "null",
+        retmode = "xml",
+        id = paste(taxid, collapse = ",")
+    )
 
-  query=list(db='taxonomy', rettype='null', retmode='xml', id=paste(taxid, collapse=','))
+    response <- POST_retry(fetch_url, body = query)
 
-  response = POST_retry(fetch_url, body=query)
+    #stop if response failed
+    stop_for_status(response)
 
-  #stop if response failed
-  stop_for_status(response)
-
-  parse_taxonomy_xml(xmlParse(content(response, as="text")))
+    parse_taxonomy_xml(XML::xmlParse(content(response, as = "text")))
 }
-parse_taxonomy_xml = function(xml){
-  rbind.fill(xpathApply(xml, '//TaxaSet/Taxon', parse_taxon))
+parse_taxonomy_xml <- function(xml) {
+    plyr::rbind.fill(XML::xpathApply(xml, "//TaxaSet/Taxon", parse_taxon))
 }
-parse_taxon = function(taxon){
-  tax_id = xpathSApply(taxon, './TaxId', xmlValue)
-  ranks = xpathSApply(taxon, './/Rank', xmlValue)
-  names = xpathSApply(taxon, './/ScientificName', xmlValue)
-  names(names) = ranks
-  names = names[ranks != 'no rank']
-  names['taxId'] = tax_id
-  as.data.frame(t(names), stringsAsFactors=FALSE)
+parse_taxon <- function(taxon) {
+    tax_id <- XML::xpathSApply(taxon, "./TaxId", xmlValue)
+    ranks <- XML::xpathSApply(taxon, ".//Rank", xmlValue)
+    names <- XML::xpathSApply(taxon, ".//ScientificName", xmlValue)
+    names(names) <- ranks
+    names <- names[ranks != "no rank"]
+    names["taxId"] <- tax_id
+    as.data.frame(t(names), stringsAsFactors = FALSE)
 }
